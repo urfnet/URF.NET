83da93775dbe80baf12edf56b459e3132465f8dd
ifarkas_cp <SND\ifarkas_cp>
2013-09-27 15:02:08 -0700

    Discussed. Removed comments. Refactored.

--- dev/Repository/Repository.cs
+++ dev/Repository/Repository.cs
@@ -10,29 +10,14 @@ namespace Repository
 {
     public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
-        //TODO: What's the purose of _instanceId
-        // This is for testing for DI & IoC frameworks ensuring that we are getting a new instance, the test
-        // case was to ensure that the lifecyle of the UoW and DbContext was bound to the the lifetime of an 
-        // HttpRequest, so for ever user they should have the same instnance throughout their HttpRequest however
-        // the next HttpRequets they should get a new instance.
         private readonly Guid _instanceId;
-
-        internal IDbContext Context;
-
-        //TODO: Discuss - Remove comment afterward
-        //IDbSet does not define FindAsync
-        //Remarks: System.Data.Entity.IDbSet<TEntity> was originally intended to allow creation of test doubles (mocks or fakes) for System.Data.Entity.DbSet<TEntity>.
-        //However, this approach has issues in that adding new members to an interface breaks existing code that already implements the interface without the new members.
-        //Therefore, starting with EF6, no new members will be added to this interface and it is recommended that System.Data.Entity.DbSet<TEntity> be used as the base class for test doubles.
-
-        // Let's discuss this, not sure if there is a recommendation from you or not.
-        // Will setup sometime for us to discuss this
-        internal DbSet<TEntity> DbSet;
+        private readonly IDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
 
         public Repository(IDbContext context)
         {
-            Context = context;
-            DbSet = context.Set<TEntity>();
+            _context = context;
+            _dbSet = context.Set<TEntity>();
             _instanceId = Guid.NewGuid();
         }
 
@@ -41,66 +26,53 @@ namespace Repository
             get { return _instanceId; }
         }
 
-        //TODO: Discuss. Renamed to Find. FindById implies that there is one ID, but the 
-        // method takes params object[] keyValues to enable composite keys.
-        
-        // awesome, love the new name
         public virtual TEntity Find(params object[] keyValues)
         {
-            return DbSet.Find(keyValues);
+            return _dbSet.Find(keyValues);
         }
 
         public virtual async Task<TEntity> FindAsync(params object[] keyValues)
         {
-            return await DbSet.FindAsync(keyValues);
+            return await _dbSet.FindAsync(keyValues);
         }
 
         public virtual async Task<TEntity> FindAsync(CancellationToken cancellationToken, params object[] keyValues)
         {
-            return await DbSet.FindAsync(cancellationToken, keyValues);
+            return await _dbSet.FindAsync(cancellationToken, keyValues);
         }
 
         public virtual IQueryable<TEntity> SqlQuery(string query, params object[] parameters)
         {
-            return DbSet.SqlQuery(query, parameters).AsQueryable();
+            return _dbSet.SqlQuery(query, parameters).AsQueryable();
         }
 
         public virtual void InsertGraph(TEntity entity)
         {
-            DbSet.Add(entity);
+            _dbSet.Add(entity);
         }
 
         public virtual void Update(TEntity entity)
         {
-            DbSet.Attach(entity);
+            _dbSet.Attach(entity);
             ((IObjectState)entity).State = ObjectState.Modified;
-
-            //TODO: Discuss - This would eliminate the need for IObjectState, ObjectState and ApplyStateChanges()
-            // Added DbEntityEntry Entry(object o) to IDbContext
-            //Context.Entry(entity).State = EntityState.Modified;
-
-            // This is to decouple the application from EF and simply the complexity of for when a developer deals with 
-            // an entity graph. For example you may want to edit a Customer entity, edit a PhoneNumber entity,  
-            // add a PhoneNumber entity, edit an Order entity, add and remove OrderDetail all in one entity graph starting
-            // with the Customer as the root entity. The State property will allows the developer to have
         }
 
         public virtual void Delete(object id)
         {
-            var entity = DbSet.Find(id);
+            var entity = _dbSet.Find(id);
             Delete(entity);
         }
 
         public virtual void Delete(TEntity entity)
         {
-            DbSet.Attach(entity);
+            _dbSet.Attach(entity);
             ((IObjectState)entity).State = ObjectState.Deleted;
-            DbSet.Remove(entity);
+            _dbSet.Remove(entity);
         }
 
         public virtual void Insert(TEntity entity)
         {
-            DbSet.Attach(entity);
+            _dbSet.Attach(entity);
             ((IObjectState)entity).State = ObjectState.Added;
         }
 
@@ -117,7 +89,7 @@ namespace Repository
             int? page = null,
             int? pageSize = null)
         {
-            IQueryable<TEntity> query = DbSet;
+            IQueryable<TEntity> query = _dbSet;
 
             if (includeProperties != null)
             {
@@ -143,9 +115,7 @@ namespace Repository
             return query;
         }
 
-        //TODO: Altough this does not do any async operations the AsyncEntitySetController class requires this signature.
-        //Discuss!!!
-        //Will setup a meeting for us to discuss this.
+        //TODO: Check db traffic for IEnumerable<TEntity>
         internal async Task<IEnumerable<TEntity>> GetAsync(
                     Expression<Func<TEntity, bool>> filter = null,
                     Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
@@ -153,7 +123,7 @@ namespace Repository
                     int? page = null,
                     int? pageSize = null)
         {
-            IQueryable<TEntity> query = DbSet;
+            IQueryable<TEntity> query = _dbSet;
 
             if (includeProperties != null)
             {
