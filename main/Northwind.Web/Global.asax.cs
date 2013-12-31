e0f2a8a454f95ff8a5729cfa71a610eaec671f29
lelong37_cp <SND\lelong37_cp>
2013-12-30 19:17:27 -0800

    Upgraded all projects to Entity Framework 6.0.2 removed validation block and any references to it

--- main/Northwind.Web/Global.asax.cs
+++ main/Northwind.Web/Global.asax.cs
@@ -5,7 +5,6 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Validation.Mvc;
 
 #endregion
 
@@ -21,8 +20,6 @@ namespace Northwind.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            ModelValidatorProviders.Providers.Add(new FluentModelValidatorProvider());
         }
     }
 }
