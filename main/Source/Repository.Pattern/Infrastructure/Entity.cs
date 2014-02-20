3b743d792e611fa404eebb5b4bc204c8f326ed53
lelong37 <lelong37@gmail.com>
2014-02-20 15:26:19 -0600

    feature

---
+++ main/Source/Repository.Pattern/Infrastructure/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Repository.Pattern.Infrastructure
+{
+    public abstract class Entity : IObjectState
+    {
+        [NotMapped]
+        public ObjectState ObjectState { get; set; } //TODO: Renamed since a possible coflict with State entity column
+    }
+}
