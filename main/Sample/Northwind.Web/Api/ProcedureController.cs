ba90dbae3dc9bdd98cf77b120d025fcf34c16a05
lelong37 <lelong37@gmail.com>
2014-03-27 01:32:35 -0500

    Adding stored proc service pattern example

--- main/Sample/Northwind.Web/Api/ProcedureController.cs
+++ main/Sample/Northwind.Web/Api/ProcedureController.cs
@@ -9,27 +9,27 @@ using Northwind.Service;
 
 namespace Northwind.Web.Api
 {
-    public class ProcedureController : ApiController
+public class ProcedureController : ApiController
+{
+    private readonly IStoredProcedureService _storedProcedureService;
+
+    public ProcedureController(IStoredProcedureService storedProcedureService)
     {
-        private readonly IStoredProcedureService _storedProcedureService;
-
-        public ProcedureController(IStoredProcedureService storedProcedureService)
-        {
-            _storedProcedureService = storedProcedureService;
-        }
-
-        [HttpGet]
-        public HttpResponseMessage CustomerOrderHistory(string customerID)
-        {
-            var customerOrderHistory = _storedProcedureService.CustomerOrderHistory(customerID);
-            return Request.CreateResponse(HttpStatusCode.OK, customerOrderHistory);
-        }
-
-        [HttpGet]
-        public HttpResponseMessage CustomerOrderDetail(string customerId)
-        {
-            var customerOrderDetails = _storedProcedureService.CustomerOrderDetail(customerId);
-            return Request.CreateResponse(HttpStatusCode.OK, customerOrderDetails);
-        }
+        _storedProcedureService = storedProcedureService;
     }
+
+    [HttpGet]
+    public HttpResponseMessage CustomerOrderHistory(string customerID)
+    {
+        var customerOrderHistory = _storedProcedureService.CustomerOrderHistory(customerID);
+        return Request.CreateResponse(HttpStatusCode.OK, customerOrderHistory);
+    }
+
+    [HttpGet]
+    public HttpResponseMessage CustomerOrderDetail(string customerId)
+    {
+        var customerOrderDetails = _storedProcedureService.CustomerOrderDetail(customerId);
+        return Request.CreateResponse(HttpStatusCode.OK, customerOrderDetails);
+    }
+}
 }
