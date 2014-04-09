495394f725d6865a9adc47f4a4065cd3b1bebd1e
ifarkas <ivan.farkas@3dhdsoft.com>
2014-04-09 00:59:21 -0400

    Fixed DeleteAsync()

--- main/Source/Service.Pattern/Service.cs
+++ main/Source/Service.Pattern/Service.cs
@@ -1,6 +1,4 @@
-﻿#region
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -10,107 +8,53 @@ using System.Web.Http.OData.Query;
 using Repository.Pattern.Infrastructure;
 using Repository.Pattern.Repositories;
 
-#endregion
-
 namespace Service.Pattern
 {
     public abstract class Service<TEntity> : IService<TEntity> where TEntity : IObjectState
     {
+        #region Private Fields
         private readonly IRepositoryAsync<TEntity> _repository;
+        #endregion Private Fields
 
-        protected Service(IRepositoryAsync<TEntity> repository)
-        {
-            _repository = repository;
-        }
+        #region Constructor
+        protected Service(IRepositoryAsync<TEntity> repository) { _repository = repository; }
+        #endregion Constructor
 
-        public virtual TEntity Find(params object[] keyValues)
-        {
-            return _repository.Find(keyValues);
-        }
+        public virtual TEntity Find(params object[] keyValues) { return _repository.Find(keyValues); }
 
-        public virtual IQueryable<TEntity> SelectQuery(string query, params object[] parameters)
-        {
-            return _repository.SelectQuery(query, parameters).AsQueryable();
-        }
+        public virtual IQueryable<TEntity> SelectQuery(string query, params object[] parameters) { return _repository.SelectQuery(query, parameters).AsQueryable(); }
 
-        public virtual void Insert(TEntity entity)
-        {
-            _repository.Insert(entity);
-        }
+        public virtual void Insert(TEntity entity) { _repository.Insert(entity); }
 
-        public virtual void InsertRange(IEnumerable<TEntity> entities)
-        {
-            _repository.InsertRange(entities);
-        }
+        public virtual void InsertRange(IEnumerable<TEntity> entities) { _repository.InsertRange(entities); }
 
-        public virtual void InsertGraph(TEntity entity)
-        {
-            _repository.Insert(entity);
-        }
+        public virtual void InsertGraph(TEntity entity) { _repository.Insert(entity); }
 
-        public virtual void InsertGraphRange(IEnumerable<TEntity> entities)
-        {
-            _repository.InsertGraphRange(entities);
-        }
+        public virtual void InsertGraphRange(IEnumerable<TEntity> entities) { _repository.InsertGraphRange(entities); }
 
-        public virtual void Update(TEntity entity)
-        {
-            _repository.Update(entity);
-        }
+        public virtual void Update(TEntity entity) { _repository.Update(entity); }
 
-        public virtual void Delete(object id)
-        {
-            _repository.Delete(id);
-        }
+        public virtual void Delete(object id) { _repository.Delete(id); }
 
-        public virtual void Delete(TEntity entity)
-        {
-            _repository.Delete(entity);
-        }
+        public virtual void Delete(TEntity entity) { _repository.Delete(entity); }
 
-        public IQueryFluent<TEntity> Query()
-        {
-            return _repository.Query();
-        }
+        public IQueryFluent<TEntity> Query() { return _repository.Query(); }
 
-        public virtual IQueryFluent<TEntity> Query(IQueryObject<TEntity> queryObject)
-        {
-            return _repository.Query(queryObject);
-        }
+        public virtual IQueryFluent<TEntity> Query(IQueryObject<TEntity> queryObject) { return _repository.Query(queryObject); }
 
-        public virtual IQueryFluent<TEntity> Query(Expression<Func<TEntity, bool>> query)
-        {
-            return _repository.Query(query);
-        }
+        public virtual IQueryFluent<TEntity> Query(Expression<Func<TEntity, bool>> query) { return _repository.Query(query); }
 
-        public virtual async Task<TEntity> FindAsync(params object[] keyValues)
-        {
-            return await _repository.FindAsync(keyValues);
-        }
+        public virtual async Task<TEntity> FindAsync(params object[] keyValues) { return await _repository.FindAsync(keyValues); }
 
-        public virtual async Task<TEntity> FindAsync(CancellationToken cancellationToken, params object[] keyValues)
-        {
-            return await _repository.FindAsync(cancellationToken, keyValues);
-        }
+        public virtual async Task<TEntity> FindAsync(CancellationToken cancellationToken, params object[] keyValues) { return await _repository.FindAsync(cancellationToken, keyValues); }
 
-        public virtual async Task<bool> DeleteAsync(params object[] keyValues)
-        {
-            return await DeleteAsync(CancellationToken.None, keyValues);
-        }
+        public virtual async Task<bool> DeleteAsync(params object[] keyValues) { return await DeleteAsync(CancellationToken.None, keyValues); }
 
-        public virtual async Task<bool> DeleteAsync(CancellationToken cancellationToken, params object[] keyValues)
-        {
-            return await DeleteAsync(cancellationToken, keyValues);
-        }
+        //IF 3/19/2014 - Before: return await DeleteAsync(cancellationToken, keyValues);
+        public virtual async Task<bool> DeleteAsync(CancellationToken cancellationToken, params object[] keyValues) { return await _repository.DeleteAsync(cancellationToken, keyValues); }
 
-        public IQueryable ODataQueryable(ODataQueryOptions<TEntity> oDataQueryOptions)
-        {
-            return _repository.Queryable(oDataQueryOptions);
-        }
+        public IQueryable ODataQueryable(ODataQueryOptions<TEntity> oDataQueryOptions) { return _repository.Queryable(oDataQueryOptions); }
 
-        public IQueryable<TEntity> ODataQueryable()
-        {
-            return _repository.Queryable();
-        }
+        public IQueryable<TEntity> ODataQueryable() { return _repository.Queryable(); }
     }
 }
