24918ee969191dcafc0296911e87549b572bf2b9
lelong37 <lelong37@gmail.com>
2014-04-23 16:50:25 -0500

    deleting master branch

--- concepts/Trackable Entities/Source/Repository.Pattern/Query/IQueryHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Repository.Pattern.Query
-{
-    public interface IQueryHandler<in TQuery, out TResult> where TQuery : IQuery<TResult>
-    {
-        TResult Handle(TQuery query);
-    }
-}
--- releases/v1.0/Northwind.Web/Scripts/_references.js
+++
@@ -1,3 +0,0 @@
-﻿/// <reference path="jquery-2.0.3.js" />
-/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="bootstrap.js" />
