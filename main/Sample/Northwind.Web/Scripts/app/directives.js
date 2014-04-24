fc23ea4dfe4e2dce2b9626a6cb299540f3aaccb5
lelong37 <lelong37@gmail.com>
2014-04-24 18:49:39 -0500

    AngularJS filter and directives update

---
+++ main/Sample/Northwind.Web/Scripts/app/directives.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+
+angular.module('northwindApp.directives', []).
+  directive('appVersion', ['version', function (version) {
+      return function (scope, elm, attrs) {
+          elm.text(version);
+      };
+  }]);
---
+++ main/Sample/Northwind.Web/Scripts/app/filters.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+
+angular.module('northwindApp.filters', []).
+  filter('interpolate', ['version', function (version) {
+      return function (text) {
+          return String(text).replace(/\%VERSION\%/mg, version);
+      };
+  }]);
