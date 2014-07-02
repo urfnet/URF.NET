2e2d029f5876febd44430ef52012952434fdfe7b
lelong37 <lelong37@gmail.com>
2014-07-01 21:26:34 -0500

    adding customer repository integration tests

--- main/Sample/Northwind.Test/IntegrationTests/CustomerRepository_Tests.cs
+++ main/Sample/Northwind.Test/IntegrationTests/CustomerRepository_Tests.cs
@@ -5,6 +5,7 @@ using Microsoft.SqlServer.Management.Smo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Data.SqlClient;
 using Northwind.Entities.Models;
+using Northwind.Repository.Models;
 using Repository.Pattern.DataContext;
 using Repository.Pattern.Ef6;
 using Repository.Pattern.Repositories;
@@ -14,17 +15,21 @@ using Northwind.Repository.Repositories;
 namespace Northwind.Test.IntegrationTests
 {
     [TestClass]
-    public class CustomerTests
+    public class CustomerRepositoryTests
     {
         [TestInitialize]
         public void Setup()
         {
+            TestContext.WriteLine("Please copy //Sql//instnwnd.sql is copied to C:\\temp\\instnwnd.sql for test to run succesfully");
+            TestContext.WriteLine("TestFixture executing, creating NorthwindTest Db for intergration  tests");
             const string sqlConnectionString = "Data Source=.;Initial Catalog=master;Integrated Security=True";
+            TestContext.WriteLine("Loading and parsing create NorthwindTest database Sql script");
             var file = new FileInfo("C:\\temp\\instnwnd.sql");
             var script = file.OpenText().ReadToEnd();
             var connection = new SqlConnection(sqlConnectionString);
             var server = new Server(new ServerConnection(connection));
             server.ConnectionContext.ExecuteNonQuery(script);
+            TestContext.WriteLine("NorthwindTest Db created for intergration tests");
         }
 
         [TestCleanup]
@@ -34,15 +39,19 @@ namespace Northwind.Test.IntegrationTests
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void GetCustomerOrderTest()
         {
             using (IDataContextAsync context = new NorthwindContext())
             using (IUnitOfWorkAsync unitOfWork = new UnitOfWork(context))
             {
                 IRepositoryAsync<Customer> customerRepository = new Repository<Customer>(context, unitOfWork);
                 var customerOrders = customerRepository.GetCustomerOrder("USA");
-                Assert.IsTrue(customerOrders.Any());
+                var enumerable = customerOrders as CustomerOrder[] ?? customerOrders.ToArray();
+                TestContext.WriteLine("Customers found: {0}", enumerable.Count());
+                Assert.IsTrue(enumerable.Any());
             }
         }
+
+        public TestContext TestContext { get; set; }
     }
 }
